eb2f05e76eb369a07db68b695623b664351d0528
SahanRachintha <sahanrachintha@gmail.com>
2024-12-25 17:53:12 +0530

    log in and sign up page

---
+++ index.js
@@ -0,0 +1,9 @@
+function showSignupForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
+
+function showLoginForm() {
+    document.getElementById('signup-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'block';
+}
